933122ad350da3a01cbace14cab942f898fb6fd0
saifoelloh <saifoelloh@gmail.com>
2019-06-20 01:44:41 +0700

    remove method from module

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -1,53 +1,10 @@
-import React, { Component } from 'react'
-import { Col } from 'reactstrap'
-import Swal from 'sweetalert2';
-import Axios from 'axios';
+import React, { Component } from 'react';
+import { Col } from 'reactstrap';
 
 class Module extends Component {
-    clickHandler = data => {
-        Swal.fire({
-            type: 'question',
-            title: data.name,
-            text: 'Are you sure ??',
-            showCancelButton: true,
-            cancelButtonColor: '#ffc107',
-        })
-        .then(res => {
-            if (res.value) {
-                console.log(data);
-                Axios.get(`http://data.url`)
-                .then(res => {
-                    if (res.data.success) {
-                        Swal.fire({
-                            type: 'success',
-                            title: 'Congratulation!!',
-                            text: res.data.message
-                        })
-                    } else {
-                        Swal.fire({
-                            type: 'warning',
-                            title: 'Oops..',
-                            text: res.data.message
-                        })
-                    }
-                    
-                })
-                .catch(res=>{
-                    Swal.fire({
-                        type: 'warning',
-                        title: 'Oops..',
-                        text: res.data.message
-                    })
-                })
-            }
-        })
-    }
     render() {
         return (
-            <Col
-                className="p-0 text-center h-100"
-                style={{backgroundImage: `url(${this.props.data.background})`, cursor: 'pointer'}}
-                onClick={()=>this.clickHandler(this.props.data)} />
+            <Col className="p-0 text-center h-100" style={{backgroundImage: `url(${this.props.data.background})`, cursor: 'pointer'}}/>
         )
     }
 }
